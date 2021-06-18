9d173857454291bfe6b5a4bf157127117829f95e
suvorov <outtotem@gmail.com>
2021-06-18 14:11:09 +0300

    two-sum: test.py: Unit-test refactoring. Added test for DictSolution.

--- python/1.Two-sum/test.py
+++ python/1.Two-sum/test.py
@@ -3,7 +3,7 @@
 import unittest
 import random
 
-from run import Solution
+from run import Solution, DictSolution
 
 class SolutionTest(unittest.TestCase):
 
@@ -11,29 +11,52 @@ class SolutionTest(unittest.TestCase):
         self.solution = Solution()
         self.random_loops = 10000
 
+    def indices(self, nums, target):
+        indices = self.solution.twoSum(nums, target)
+
+        if indices is None:
+            return []
+
+        return indices
+
+    def values(self, nums, target):
+        indices = self.indices(nums, target)
+
+        if indices is None:
+            return []
+
+        values = [nums[idx] for idx in indices]
+        return values
+
+    def values_sum(self, nums, target):
+        return sum(self.values(nums, target))
+
     def test_uniform(self):
-        self.assertEqual(
-            self.solution.twoSum([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 9),
-            [0, 7]
-        )
+        nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        target = 9
+
+        self.assertIsNotNone(self.indices(nums, target))
+        self.assertEqual(self.values_sum(nums, target), target)
 
     def test_zero(self):
-        self.assertEqual(
-            self.solution.twoSum([9, 0, 2, 7], 9),
-            [0, 1]
-        )
+        nums = [9, 0, 2, 7]
+        target = 9
+
+        self.assertIsNotNone(self.indices(nums, target))
+        self.assertEqual(self.values_sum(nums, target), target)
 
     def test_repeat(self):
-        self.assertEqual(
-            self.solution.twoSum([3, 0, 2, 7], 9),
-            [2, 3]
-        )
+        nums = [3, 0, 2, 7]
+        target = 9
+
+        self.assertIsNotNone(self.indices(nums, target))
+        self.assertEqual(self.values_sum(nums, target), target)
 
     def test_empty(self):
-        self.assertEqual(
-            self.solution.twoSum([3, 0, 1, 7], 9),
-            None
-        )
+        nums = [3, 0, 1, 7]
+        target = 9
+
+        self.assertIsNone(self.solution.twoSum(nums, target))
 
     def test_random(self):
         for i in range(0, self.random_loops):
@@ -46,7 +69,6 @@ class SolutionTest(unittest.TestCase):
             indices = self.solution.twoSum(rand_nums, rand_target)
 
             if indices is None:
-                #print("No result")
                 continue
 
             # Get values by indices
@@ -56,6 +78,11 @@ class SolutionTest(unittest.TestCase):
 
             # Check function result
             self.assertEqual(sum(values), rand_target)
+
+class DictSolutionTest(SolutionTest):
+    def setUp(self):
+        self.solution = DictSolution()
+        self.random_loops = 10000
 
 if __name__ == "__main__":
     unittest.main()
