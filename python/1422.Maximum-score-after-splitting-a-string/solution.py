4231cb81e589575d97589f650d07e75d6864ed03
Polygaev <a.polygaev@gmail.com>
2023-12-22 21:06:53 +0300

    Python: 1422. Maximum Score After Splitting a String: Add O(2n)

--- python/1422.Maximum-score-after-splitting-a-string/solution.py
+++ python/1422.Maximum-score-after-splitting-a-string/solution.py
@@ -1,4 +1,6 @@
 class Solution:
+
+    # O(n^2)
     def maxScore(self, s: str) -> int:
 
         max_score = 0
@@ -10,6 +12,36 @@ class Solution:
 
             l_score = l.count("0")
             r_score = r.count("1")
+
+            score = l_score + r_score
+
+            if score > max_score:
+                max_score = score
+
+        return max_score
+
+    # O(2n)
+    def maxScore2(self, s: str) -> int:
+
+        len_s = len(s)
+        ones_total = s.count("1")
+
+        l_score = 0
+        r_score = ones_total
+
+        if s[0] == "0":
+            l_score += 1
+        elif r_score >= 1:
+            r_score -= 1
+
+        max_score = l_score + r_score
+
+        for i in range(1, len_s - 1):
+
+            if s[i] == "0":
+                l_score += 1
+            else:
+                r_score -= 1
 
             score = l_score + r_score
 
